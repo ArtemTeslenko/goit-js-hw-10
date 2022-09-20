94a7b4731b46910f75b45e87aa7dd49407e5c431
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2022-09-20 21:55:46 +0300

    Add deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import "./css/styles.css";
-import { fetchCountries } from "./js/fetchCountries";
-import * as renderingFn from "./js/rendering";
+import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import * as renderingFn from './js/rendering';
 
-const debounce = require("lodash.debounce");
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-const inputEl = document.querySelector("#search-box");
+const inputEl = document.querySelector('#search-box');
 
-inputEl.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
   const val = e.target.value.trim();
